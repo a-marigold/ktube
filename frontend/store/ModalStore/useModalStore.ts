1b239021c6b8c95d518e580d367423b60d32899e
NextKolya <phelixgluy@gmail.com>
2025-12-16 01:18:51 +0600

    Delete setCurrentModal. Add open and close modal instead

--- frontend/store/ModalStore/useModalStore.ts
+++ frontend/store/ModalStore/useModalStore.ts
@@ -4,8 +4,16 @@ import { create } from 'zustand';
 
 interface ModalStore {
     currentModal: ReactNode;
+
+    openModal: (modal: ReactNode) => void;
+
+    closeModal: () => void;
 }
 
 export const useModalStore = create<ModalStore>()((set) => ({
     currentModal: null,
+
+    openModal: (modal) => set({ currentModal: modal }),
+
+    closeModal: () => set({ currentModal: null }),
 }));
