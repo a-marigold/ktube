d23bfed13c12c9b507b748ed848bb76eb6fedd41
NextKolya <phelixgluy@gmail.com>
2025-12-16 16:38:52 +0600

    Add MenuLink UI component

---
+++ frontend/UI/MenuLink/MenuLink.tsx
@@ -0,0 +1,38 @@
+import Link, { LinkProps } from 'next/link';
+
+import type { SvgIconProps } from '@/types/SvgIconProps';
+
+import linkStyles from './MenuLink.module.scss';
+
+export interface MenuLinkProps extends LinkProps {
+    title: string;
+
+    icon: SvgIconProps;
+
+    className?: string;
+}
+export default function MenuLink({
+    title,
+    icon,
+
+    className,
+
+    ...attributes
+}: MenuLinkProps) {
+    return (
+        <Link
+            {...attributes}
+            className={`${linkStyles['menu-link']} ${className ?? ''}`}
+        >
+            <svg
+                width={icon.width}
+                height={icon.height}
+                color={icon.color || 'var(--font-color)'}
+            >
+                <use href={icon.href} />
+            </svg>
+
+            <span className={linkStyles['title']}>{title}</span>
+        </Link>
+    );
+}
