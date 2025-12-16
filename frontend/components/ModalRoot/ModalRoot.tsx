d0e4a907e9865dd9a150e560a53ae6c691af6ab6
NextKolya <phelixgluy@gmail.com>
2025-12-16 15:51:40 +0600

    add scroll locking in ModalRoot

--- frontend/components/ModalRoot/ModalRoot.tsx
+++ frontend/components/ModalRoot/ModalRoot.tsx
@@ -1,9 +1,23 @@
 'use client';
 
+import { useEffect } from 'react';
+
 import { useModalStore } from '@/store/ModalStore';
+
+import { lockBodyScroll, unlockBodyScroll } from '@/utils/lockBodyScroll';
 
 export default function ModalRoot() {
     const currentModal = useModalStore((state) => state.currentModal);
+
+    useEffect(() => {
+        if (currentModal) {
+            lockBodyScroll();
+        }
+
+        return () => {
+            unlockBodyScroll();
+        };
+    }, [currentModal]);
 
     return currentModal;
 }
