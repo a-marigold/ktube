16fe15748fb061ced05d7e93e47503888475f4c4
NextKolya <phelixgluy@gmail.com>
2026-01-06 16:03:19 +0600

    add OverlayButton UI component

---
+++ frontend/UI/OverlayButton/OverlayButton.tsx
@@ -0,0 +1,38 @@
+import type { ButtonHTMLAttributes } from 'react';
+
+import type { IconProps } from '@/types/IconProps';
+
+import buttonStyles from './OverlayButton.module.scss';
+
+interface OverlayButtonProps
+    extends ButtonHTMLAttributes<HTMLButtonElement>,
+        IconProps {
+    shape: 'circle' | 'ellipse';
+    'aria-label': string;
+}
+export default function OverlayButton({
+    iconHref,
+
+    iconWidth,
+    iconHeight,
+    iconColor,
+
+    shape,
+
+    className,
+
+    ...attributes
+}: OverlayButtonProps) {
+    return (
+        <button
+            {...attributes}
+            className={`${buttonStyles['overlay-button']} ${
+                buttonStyles[shape]
+            } ${className ?? ''}`}
+        >
+            <svg width={iconWidth} height={iconHeight} color={iconColor}>
+                <use href={iconHref} />
+            </svg>
+        </button>
+    );
+}
