7dd7cf9a44d6c390db528fae3ada37d8864a5624
NextKolya <phelixgluy@gmail.com>
2026-01-15 20:41:04 +0600

    add build shared step in frontend ci

--- backend/src/routes/auth/auth.controller.ts
+++ backend/src/routes/auth/auth.controller.ts
@@ -1,5 +1,7 @@
 import type { RouteHandler, RouteRequest } from 'bun-crumb';
 
+import { GOOGLE_OAUTH_ENDPOINT } from '../../constants/oauth';
+
 export const authorizeWithGoogle: RouteHandler = (request, response) => {
     return response.redirect('', 302);
 };
