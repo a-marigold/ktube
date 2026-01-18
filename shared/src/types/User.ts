ab1b319988841a625fe5172c17420a4d84e8291f
NextKolya <phelixgluy@gmail.com>
2026-01-18 12:49:32 +0600

    Update User type in shared

--- shared/src/types/User.ts
+++ shared/src/types/User.ts
@@ -1,6 +1,11 @@
 import { string, object } from 'zod/v4-mini';
 import type { infer as zInfer } from 'zod/v4-mini';
 
-export const userSchema = object({ name: string() });
+export const userSchema = object({
+    sub: string(),
+    name: string(),
+    picture: string(),
+    email: string(),
+});
 
 export type User = zInfer<typeof userSchema>;
