da9994595c824eb74dc3813ab33f1e99771dc5c8
NextKolya <phelixgluy@gmail.com>
2026-01-24 14:48:35 +0600

    update tabs list in Setting Navbar

--- frontend/app/settings/[[...setting]]/components/Navbar/Navbar.tsx
+++ frontend/app/settings/[[...setting]]/components/Navbar/Navbar.tsx
@@ -1,26 +1,37 @@
+import type { ReactNode } from 'react';
+
+import { settingContents } from '../../settingContents';
+import type { Setting } from '../../types';
+
 import SettingLink from '@/UI/SettingLink';
 
 import navStyles from './Navbar.module.scss';
 
 interface NavbarProps {
-    currentSetting?: string;
+    currentSetting?: Setting;
 }
 export default function Navbar({ currentSetting }: NavbarProps) {
-    const tabs = ['Account'];
+    const tabs: ReactNode[] = [];
+
+    for (const setting in settingContents) {
+        const content = settingContents[setting];
+
+        tabs.push(
+            <SettingLink
+                key={setting}
+                href={'/settings/' + setting}
+                aria-label={'Go to the ' + content.name + ' settings'}
+                isActive={setting === currentSetting}
+                title={content.name}
+            />,
+        );
+    }
 
     return (
         <nav className={navStyles['navbar']}>
             <span className={navStyles['title']}>Settings</span>
 
-            {tabs.map((tab) => (
-                <SettingLink
-                    href={'/settings/' + tab}
-                    key={tab}
-                    aria-label={`Go to the ${tab} settings`}
-                    isActive={tab === currentSetting}
-                    title={tab}
-                />
-            ))}
+            {tabs}
         </nav>
     );
 }
